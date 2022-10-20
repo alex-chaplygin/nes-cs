e37246931a2d56bd1ba469eb0491810a7e071f33
alex-chaplygin <alex_chaplygin@mail.ru>
2022-10-20 07:26:53 +0300

    Документация, функция GetChrBank

--- NES/Cartridge.cs
+++ NES/Cartridge.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,37 +7,102 @@ using System.IO;
 
 namespace NES
 {
+    /// <summary>
+    /// класс картриджей
+    /// </summary>
     public class Cartridge
     {
+        /// <summary>
+        /// число банков программ
+        /// </summary>
         public static int prg_count;
+
+        /// <summary>
+        /// число банков изображений
+        /// </summary>
         public static int chr_count;
+
+        /// <summary>
+        /// зеркальное отображение: горизонтальное (вертикальное расположение)
+        /// </summary>
         public static bool mirroring;
+
+        /// <summary>
+        /// если значение true, то трейнер присутствует
+        /// </summary>
         public static bool trainer;
+
+        /// <summary>
+        /// картридж содержит RPG RAM с батарейным питанием или другую постоянную память
+        /// </summary>
         public static bool prg_ram;
+
+        /// <summary>
+        /// Игнорировать управление зеркалированием или бит выше зеркалирования
+        /// </summary>
         public static bool ignore_mirroring;
+
+        /// <summary>
+        /// номер маппера
+        /// </summary>
         public static int mapper;
+
+        /// <summary>
+        /// память программ
+        /// </summary>
         static byte[] prg_mem;
+
+        /// <summary>
+        /// память изображений
+        /// </summary>
         static byte[] chr_mem;
+
+        /// <summary>
+        /// память трейнера
+        /// </summary>
         static byte[] trainer_mem;
 
+        /// <summary>
+        /// получить банк программы
+        /// </summary>
+        /// <param name="n">номер банка</param>
+        /// <returns>банк программы</returns>
         public static byte[] GetPrgBank(int n)
         {
-            return prg_mem.Skip<byte>(n * Memory.PRG_SIZE).
-		Take(Memory.PRG_SIZE).ToArray();
+            if (n < 0 || n >= prg_count)
+		return null;
+            return prg_mem.Skip<byte>(n * Memory.PRG_SIZE).Take(Memory.PRG_SIZE).ToArray();
         }
 
+        /// <summary>
+        /// получить банк изображений
+        /// </summary>
+        /// <param name="n">номер банка</param>
+        /// <returns>банк изображений</returns>
+        public static byte[] GetChrBank(int n)
+        {
+            if (n < 0 || n >= chr_count)
+		return null;
+            return chr_mem.Skip<byte>(n * Memory.CHR_SIZE).Take(Memory.CHR_SIZE).ToArray();
+        }
+
+        /// <summary>
+        /// прочитать файл картриджа
+        /// </summary>
+        /// <param name="fileName">имя файла</param>
+        /// <returns>true - если правильный NES файл, иначе false</returns>
         public static bool ReadFile(string fileName)
         {
-	    byte[] Header = { 0x4E, 0x45, 0x53, 0x1A };
+	        byte[] Header = { 0x4E, 0x45, 0x53, 0x1A };
             byte[] header;
             using (var stream = File.Open(fileName, FileMode.Open))
-	    {
-		using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
-		{
-		    header = reader.ReadBytes(16);
-		    for (int i = 0; i < 4; i++)
-			if (header[i] != Header[i])
-			    return false;
+	        {
+		        using (var reader = new BinaryReader(stream, Encoding.UTF8, false))
+		        {
+		            header = reader.ReadBytes(16);
+		            for (int i = 0; i < 4; i++)
+			            if (header[i] != Header[i])
+			                return false;
                     prg_count = header[4];
                     prg_mem = new byte[prg_count * Memory.PRG_SIZE];
                     chr_count = header[5];
