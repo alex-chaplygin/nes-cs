a8ac1820fa570c7cd939d91804f5775ff518ac57
A1Asriel <27139654+A1Asriel@users.noreply.github.com>
2022-10-21 23:17:47 +0300

    fix: use actual data for tests

--- NESTest/CartridgeTest.cs
+++ NESTest/CartridgeTest.cs
@@ -11,10 +11,10 @@ namespace NESTest {
             Assert.AreEqual(expectedTrainerMem, trainerMem);
         }
 
-        [TestMethod]
+        /*[TestMethod]
         public void TetrisTest()
         {
-            byte[] chrMem = {0x38, 0x4c, 0xc6, 0xc6, 0xc6, 0x64, 0x38, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
+            byte[] chrMem = {};
             byte[] trainerMem = {};
             Test(@"..\..\..\Tetris.nes", chrMem, trainerMem);
         }
@@ -25,12 +25,12 @@ namespace NESTest {
             byte[] chrMem = {};
             byte[] trainerMem = {};
             Test(@"..\..\..\Castlevania.nes", chrMem, trainerMem);
-        }
+        }*/
 
         [TestMethod]
         public void SMBTest()
         {
-            byte[] chrMem = {};
+            byte[] chrMem = {0x03, 0x0F, 0x1F, 0x1F, 0x1C, 0x24, 0x26, 0x66, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x3F, 0x3F, 0x7F};
             byte[] trainerMem = {};
             Test(@"..\..\..\Super_mario_brothers.nes", chrMem, trainerMem);
         }
