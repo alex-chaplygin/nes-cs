ae8833912d17733360be1e56108f1f8671737ac5
alex-chaplygin <alex_chaplygin@mail.ru>
2022-10-26 17:04:19 +0300

    Новые модули

---
+++ NES/Input.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES
+{
+    internal class Input
+    {
+        public static byte Read(ushort adr)
+        {
+            return 0;
+        }
+        public static void Write(ushort adr, byte value)
+        {
+
+        }
+    }
+}
---
+++ NES/PPU.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES
+{
+    internal class PPU
+    {
+        public static void Write(ushort adr, byte value)
+        {
+
+        }
+        public static byte Read(ushort adr)
+        {
+            return 0;
+        }
+    }
+}
