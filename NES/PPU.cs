c3ea44400b68b68c19028e83cb2ce82af70263f1
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-11-02 07:05:27 +0300

    Функция записи адреса

--- NES/PPU.cs
+++ NES/PPU.cs
@@ -6,15 +6,54 @@ using System.Threading.Tasks;
 
 namespace NES
 {
-    internal class PPU
-    {
+    
+    /// <summary>
+    ///   Графический процессор
+    /// </summary>
+    public class PPU
+    {	
+	/// <summary>
+	///   Регистр адреса
+	/// </summary>
+	public static ushort address;
+
+	/// <summary>
+	///   Первая запись в адрес
+	/// </summary>
+        static bool isFirst = true;
+	
+	/// <summary>
+	///   Запись в регистр PPU
+	/// </summary>
         public static void Write(ushort adr, byte value)
         {
 
         }
+
+	/// <summary>
+	///   Чтение из регистра PPU
+	/// </summary>
         public static byte Read(ushort adr)
         {
             return 0;
         }
+
+	/// <summary>
+	///   Записать адрес, сначала старший байт, затем младший
+	/// </summary>
+	public static void SetAddress(byte val)
+        {
+            if (isFirst)
+            {
+                address = (ushort)(val << 8 | (address & 0xFF));
+                isFirst = false;
+            }
+            else
+            {
+                address = (ushort)(address & 0xFF00);
+                address = (ushort)(address + val);
+                isFirst = true;
+            }
+        }	
     }
 }
