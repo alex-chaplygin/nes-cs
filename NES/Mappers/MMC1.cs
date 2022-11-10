d83c17cf8b43dcf76c5cc00d63334e783e7f53e3
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-11-10 09:31:11 +0300

    Сдвиг регистра

--- NES/Mappers/MMC1.cs
+++ NES/Mappers/MMC1.cs
@@ -11,8 +11,24 @@ namespace NES
         public static byte registr = 0x10;
         public static void Write(ushort adr, byte val)
         {
-            registr >>= 1;
-            val &= 1;
+            if (val == (val & 0x80))
+                registr = 0x10;
+            else if ((registr & 1) > 0)
+            {
+                registr >>= 1;
+                val &= 1;
+                val <<= 4;
+                registr |= val;
+                // переключение
+                registr = 0x10;
+            }
+            else
+            {
+                registr >>= 1;
+                val &= 1;
+                val <<= 4;
+                registr |= val;
+            }
         }
     }
 }
