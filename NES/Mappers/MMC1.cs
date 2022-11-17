8eb14b8252ed8b437eec42044a403cafda88ff13
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-11-17 08:23:48 +0300

    Тестирование получения номера банка

--- NES/Mappers/MMC1.cs
+++ NES/Mappers/MMC1.cs
@@ -6,20 +6,37 @@ using System.Threading.Tasks;
 
 namespace NES
 {
-    class MMC1
-	{
+    public class MMC1
+    {
+        delegate void SwitchOp();
+        struct SwitchReg
+        {
+            public int down;
+            public SwitchOp bank;
+            public SwitchReg(int u, SwitchOp r)
+            {
+                down = u;
+                bank = r;
+            }
+        }
+       
+        /// <summary>
+        /// Номер банка для переключения 
+        /// </summary>
         public static byte registr = 0x10;
         public static void Write(ushort adr, byte val)
         {
             if (val == (val & 0x80))
                 registr = 0x10;
-            else if ((registr & 1) > 0)
+             else if ((registr & 1) > 0)
             {
                 registr >>= 1;
                 val &= 1;
                 val <<= 4;
                 registr |= val;
+                Console.WriteLine($"val = {val:x} reg = {registr:X}");
                 // переключение
+                Switch(adr);
                 registr = 0x10;
             }
             else
@@ -28,7 +45,35 @@ namespace NES
                 val &= 1;
                 val <<= 4;
                 registr |= val;
+                Console.WriteLine($"val = {val:x} reg = {registr:X}");
+             
             }
+        }
+
+        public static void Chr0()
+        {
+
+        }
+        public static void Chr1()
+        {
+
+        }
+        public static void Prg()
+        {
+
+        }
+        public static void Switch(ushort adr)
+        {
+            SwitchReg[] switchTable = new SwitchReg[]
+            {
+                    new SwitchReg( 0xbfff, Chr0 ),
+                    new SwitchReg (0xffff, Chr1 ),
+                    new SwitchReg( 0xffff, Prg )
+            };
+
+            for (int i = 0; i < switchTable.Length; i++)
+                if (adr < switchTable[i].down)
+                    switchTable[i].bank();
         }
     }
 }
---
+++ NESTest/MMC1Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NES;
+
+namespace NESTest
+{
+    [TestClass]
+    public class MMC1Test
+    {
+        
+        [TestMethod]
+        public void Write()
+        {
+            Cartridge.mapper = 1;
+            Memory.Write(0xE000, 0x80);
+            byte a = 0xF;
+            for (int i = 0; i < 5; i++) 
+            {
+                Memory.Write(0xE000, a);
+                a >>= 1;
+            }
+            Assert.AreEqual(0xF, MMC1.registr);
+        }
+    }
+}
