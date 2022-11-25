df1311358a1cdb487e08f53b8bd40a57c71c4529
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-11-25 09:21:37 +0300

    Новый проект - тест видео

---
+++ VideoTest/Form1.Designer.cs
@@ -0,0 +1,50 @@
+﻿
+namespace VideoTest
+{
+    partial class form11
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // form11
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 442);
+            this.Name = "form11";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ VideoTest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NES;
+
+namespace VideoTest
+{
+    public partial class form11 : Form
+    {
+        //byte[] pixelArray = new byte[256 * 240*3];
+        public form11()
+        {            
+            InitializeComponent();
+            //for (int i = 0; i < 256 * 240 * 3; i++)
+            //    if (i % 3 == 2) pixelArray[i] = 200;
+            PPU.MaskWrite(0xFF);
+        }
+      
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            unsafe
+            {
+                fixed (byte* ptr = PPU.GetScreen())
+                {
+                    Image bmp = new Bitmap(256, 240, 256 * 3, System.Drawing.Imaging.PixelFormat.Format24bppRgb, new IntPtr(ptr));
+                    e.Graphics.DrawImage(bmp, new Rectangle(0, 0, Width, Height));
+                }
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+
+}
