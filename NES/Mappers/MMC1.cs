67854f844babe712d707d1981194b07d2a4723f4
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-11-29 18:06:50 +0300

    Исправление свига регистров, система переключения банков

--- NES/Mappers/MMC1.cs
+++ NES/Mappers/MMC1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,27 +6,40 @@ using System.Threading.Tasks;
 
 namespace NES
 {
+    public enum Banks
+    {
+        Chr0,
+        Chr1,
+        Prg
+    }
     public class MMC1
     {
         delegate void SwitchOp();
         struct SwitchReg
         {
+            public int up;
             public int down;
             public SwitchOp bank;
-            public SwitchReg(int u, SwitchOp r)
+            public SwitchReg(int p ,int u, SwitchOp r)
             {
+                up = p;
                 down = u;
                 bank = r;
             }
         }
+
        
+
         /// <summary>
         /// Номер банка для переключения 
         /// </summary>
         public static byte registr = 0x10;
+        public static int bank;
+        public static Banks sw;
         public static void Write(ushort adr, byte val)
         {
-            if (val == (val & 0x80))
+            Console.WriteLine($"val = {val:x}");
+            if ((val & 0x80) > 0)
                 registr = 0x10;
              else if ((registr & 1) > 0)
             {
@@ -34,7 +47,7 @@ namespace NES
                 val &= 1;
                 val <<= 4;
                 registr |= val;
-                Console.WriteLine($"val = {val:x} reg = {registr:X}");
+                Console.WriteLine($"switch val = {val:x} reg = {registr:X}");
                 // переключение
                 Switch(adr);
                 registr = 0x10;
@@ -52,27 +65,31 @@ namespace NES
 
         public static void Chr0()
         {
-
+            sw = Banks.Chr0;
+            Console.WriteLine(sw);
         }
         public static void Chr1()
         {
-
+            sw = Banks.Chr1;
+            Console.WriteLine(sw);
         }
         public static void Prg()
         {
-
+            sw = Banks.Prg;
+            Console.WriteLine(sw);
         }
         public static void Switch(ushort adr)
         {
+            bank = registr; 
             SwitchReg[] switchTable = new SwitchReg[]
             {
-                    new SwitchReg( 0xbfff, Chr0 ),
-                    new SwitchReg (0xffff, Chr1 ),
-                    new SwitchReg( 0xffff, Prg )
+                    new SwitchReg( 0xA000, 0xBFFF, Chr0 ),
+                    new SwitchReg (0xC000, 0xDFFF, Chr1 ),
+                    new SwitchReg(0xE000, 0xFFFF, Prg )
             };
 
             for (int i = 0; i < switchTable.Length; i++)
-                if (adr < switchTable[i].down)
+                if (switchTable[i].up <= adr && adr  <= switchTable[i].down)
                     switchTable[i].bank();
         }
     }
