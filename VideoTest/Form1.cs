3b736e97ca1b7822d76e0de227697929824d1ff9
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-12-01 06:47:26 +0300

    Тестирование плиток, палитры, экранов

--- VideoTest/Form1.cs
+++ VideoTest/Form1.cs
@@ -14,13 +14,78 @@ namespace VideoTest
 {
     public partial class form11 : Form
     {
-        //byte[] pixelArray = new byte[256 * 240*3];
         public form11()
         {            
             InitializeComponent();
-            //for (int i = 0; i < 256 * 240 * 3; i++)
-            //    if (i % 3 == 2) pixelArray[i] = 200;
-            PPU.MaskWrite(0xFF);
+	    Width = 768 + 15;
+            Height = 720 + 37;
+        
+            PPU.MaskWrite(0xFE);
+            Memory.Write(0x2000, 19);
+            SetupTiles();
+            SetupPalette();
+            SetupNames();	    
+        }
+
+	static void SetupTiles()
+        {
+            byte[] tile_bytes = new byte[] {0x41, 0xC2, 0x44, 0x48, 0x10, 0x20, 0x40, 0x80,
+            0x01, 0x02, 0x04, 0x08, 0x16, 0x21, 0x42, 0x87};
+
+            Memory.Write(0x2006, 0);
+            Memory.Write(0x2006, 0);
+
+            for (int i = 0; i < tile_bytes.Length; i++)
+                Memory.Write(0x2007, tile_bytes[i]);
+            
+            string fileName = @"Super_mario_brothers.nes";
+            if (!Cartridge.ReadFile(fileName))
+            {
+                Console.WriteLine("File not found");
+
+            }
+            PPU.WritePattern0(Cartridge.GetChrBank(0));
+        }
+	
+        static void SetupPalette()
+        {
+            byte[] palette_bytes = new byte[] { 0x03, 0x28, 0x15, 0x23 };
+
+            Memory.Write(0x2006, 0x3F);
+            Memory.Write(0x2006, 0x00);
+
+            for (int i = 0; i < palette_bytes.Length; i++)
+                Memory.Write(0x2007, palette_bytes[i]);
+        }
+	
+        static void SetupNames()
+        {
+            Memory.Write(0x2006, 0x20);
+            Memory.Write(0x2006, 0x00);
+
+            for (int y = 0; y < 30; y++)
+                for (int x = 0; x < 32; x++)
+                    if (x < 8 && y < 8) 
+                        Memory.Write(0x2007, (byte) (x + 8 * y));
+                    else
+                        Memory.Write(0x2007, (byte) 0);
+            for (int y = 0; y < 64; y++)
+                Memory.Write(0x2007, (byte)0);
+            for (int y = 0; y < 30; y++)
+                for (int x = 0; x < 32; x++)
+                    Memory.Write(0x2007, (byte)(x+y));
+            for (int y = 0; y < 64; y++)
+                Memory.Write(0x2007, (byte)0);
+            for (int y = 0; y < 30; y++)
+                for (int x = 0; x < 32; x++)
+                    Memory.Write(0x2007, 2);
+            for (int y = 0; y < 64; y++)
+                Memory.Write(0x2007, (byte)0);
+            for (int y = 0; y < 30; y++)
+                for (int x = 0; x < 32; x++)
+                    Memory.Write(0x2007, 3);
+            for (int y = 0; y < 64; y++)
+                Memory.Write(0x2007, (byte)0);
         }
       
         private void Form1_Paint(object sender, PaintEventArgs e)
