56eb1d15460db7ade06da29660157a79bf5464c0
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-12-07 06:35:01 +0300

    Методы нажатия и отпускания клавиш

--- NES/Input.cs
+++ NES/Input.cs
@@ -6,15 +6,41 @@ using System.Threading.Tasks;
 
 namespace NES
 {
+    public enum Key 
+    {
+        A,
+        B,
+        Select,
+        Start,
+        Up,
+        Down,
+        Left,
+        Right
+    }
+    
     internal class Input
     {
+	static byte keys = 0;
+	
         public static byte Read(ushort adr)
         {
             return 0;
         }
+	
         public static void Write(ushort adr, byte value)
         {
 
+        }
+
+	public static void KeyDown(Key key)
+        {
+            keys = (byte)(keys | (byte)(1 << (int)key));
+        }
+	
+        public static void KeyUp(Key key)
+        {
+            byte temp = (byte)(1 << (int)key);
+            keys = (byte)(keys & ~temp);
         }
     }
 }
