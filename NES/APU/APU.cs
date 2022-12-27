89edd2d82a993fbf7cb60c66764bfc511374446b
Чаплыгин Александр <alex_chaplygin@mail.ru>
2022-12-27 12:31:52 +0300

    Новый класс аудио процессора

---
+++ NES/APU/APU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES.APU
+{
+    class APU
+    {
+        /// <summary>
+        /// Функцмя записи в APU 
+        /// </summary>
+        /// <param name="adr">Адрес</param>
+        /// <param name="val">Значение</param>
+        delegate void APUWrite(ushort adr, byte val);
+
+        static APUWrite[] apu = new APUWrite[]
+        {
+        };
+
+
+        /// <summary>
+        /// Записать значения в APU
+        /// </summary>
+        /// <param name="adr">Адрес</param>
+        /// <param name="val">Значнение</param>
+        public static void Write(ushort adr, byte val)
+        {
+            
+
+        }        
+    }
+}
