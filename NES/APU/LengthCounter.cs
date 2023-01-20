e91e938e4f8e3d2e85a7917f697dbaba004fdfef
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-01-20 15:53:27 +0300

    Класс счетчика

---
+++ NES/APU/LengthCounter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NES.APU
+{
+    internal class LengthCounter
+    {
+        private int value;
+        private bool isActive;
+
+        public LengthCounter(int value)
+        {
+            this.value = value;
+            this.isActive = true;
+        }
+
+        public bool Clock()
+        {
+            if (value == 0)
+                return true;
+            if (isActive && value > 0)
+            {
+                value--;
+                if (value == 0)
+                    return true;
+            }
+            return false;
+
+        }
+
+        public int GetValue()
+        {
+            return this.value;
+        }
+
+        public void Set(int value)
+        {
+            this.value = value;
+        }
+
+        public void Halt()
+        {
+            this.isActive = false;
+        }
+
+        public void Continue()
+        {
+            this.isActive = true;
+        }
+
+        public void Clear()
+        {
+            this.value = 0;
+        }
+    }
+}
