93d80917c7ebd40ae57315cc1781627191ef3ed1
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-01-21 08:03:12 +0300

    Тестирование LengthCounter

---
+++ NESTest/LengthCounterTest.cs
@@ -0,0 +1,22 @@
+﻿using NES;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NESTest
+{
+    [TestClass]
+    public class LengthCounterTest
+    {
+        [TestMethod] 
+        public void ClockTest()
+        {
+            NES.APU.LengthCounter counter = new NES.APU.LengthCounter(5);
+            Assert.AreEqual(false, counter.Clock());
+            Assert.AreEqual(4,counter.GetValue());
+            Assert.AreEqual(false,counter.Clock());
+            Assert.AreEqual(false, counter.Clock());
+            Assert.AreEqual(false, counter.Clock());
+            Assert.AreEqual(true, counter.Clock());
+            Assert.AreEqual(0, counter.GetValue());
+        }
+    }
+}
