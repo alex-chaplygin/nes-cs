c934b3cd57375701c19b57b51a2e02a15ca1e1d9
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-01-22 09:55:09 +0300

    Класс последовательности

---
+++ NESTest/SequencerTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NES;
+
+namespace NESTest
+{
+    [TestClass]
+    public class SequencerTest
+    {        
+        [TestMethod]
+        public void TestMethod()
+        {
+            int[] a = { 0, 1, 2, 3, 4 };
+            Sequencer b = new(a);
+            int c;
+
+            c = b.Clock();
+            Assert.AreEqual(0, c);
+
+            c = b.Clock();
+            Assert.AreEqual(1, c);
+
+            c = b.Clock();
+            Assert.AreEqual(2, c);
+
+            c = b.Clock();
+            Assert.AreEqual(3, c);
+
+            c = b.Clock();
+            Assert.AreEqual(4, c);
+
+            c = b.Clock();
+            Assert.AreEqual(0, c);
+        }
+    }
+}
