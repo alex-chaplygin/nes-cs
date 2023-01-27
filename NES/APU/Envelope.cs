c286da0daab8932d37f34b2fabb14228a3a21c1d
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-01-27 09:06:36 +0300

    Класс генерации огибающей

---
+++ NES/APU/Envelope.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace NES.APU
+{
+    public class Envelope
+    {
+        /// <summary>
+        /// Флаг начала
+        /// </summary>
+        static bool start_flag;
+
+        /// <summary>
+        /// Делитель
+        /// </summary>
+        Divider divider;
+
+        /// <summary>
+        /// Cчётчик угасания
+        /// </summary>
+        LengthCounter decay;
+
+        /// <summary>
+        /// Уровень громкости
+        /// </summary>
+        static int volume;
+
+        /// <summary>
+        /// Флаг постоянной громкости
+        /// </summary>
+        static bool const_volume;       
+    }
+}
